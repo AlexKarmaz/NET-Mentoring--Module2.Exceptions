170a50e87a8711f36373000b5de2ed675948c75f
AlexKarmaz <karmazct@mail.ru>
2018-03-24 22:18:57 +0300

    add task 2

---
+++ Task2.StringConverterLibrary/StringConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.StringConverterLibrary
+{
+    public static class StringConverter
+    {
+        public static int ToInt (this string str)
+        {
+            if (String.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException($"{nameof(str)} is null, empty or whitespace");
+            }
+
+            int result = 0, sign, startPosition;
+
+            startPosition = ProcessFirstSymbol(str[0],out sign);
+
+            try
+            {
+                checked
+                {
+                    foreach(char x in str.Substring(startPosition))
+                    {
+                        result = result * 10;
+                        result += GetNumber(x) * sign;
+                    }
+                }
+            }
+            catch(OverflowException ex)
+            {
+                throw new OverflowException($"{str} is too big and cannot be stored in int type", ex);
+            }
+
+            return result;
+        }
+
+        // Here is used '0' to cut all the characters except 0 to 9.
+        private static int GetNumber(char charNumber)
+        {
+            int value = charNumber - '0';
+            if (value < 0 || value > 9)
+            {
+                throw new FormatException($"The {charNumber.GetType()} value '{charNumber}' is not in a recognizable format.");
+            }
+
+            return value;
+        }
+
+
+        // Checks the first character entered. If a string begins with a number, it returns the starting position 0, 
+        // otherwise the starting position 1. Also the method returns sign of a number in out paramether.
+        private static int ProcessFirstSymbol(char firstSymbol, out int sign)
+        {
+            if (firstSymbol == '-')
+            {
+                sign = -1;
+                return 1;
+            }
+            if (firstSymbol == '+')
+            {
+                sign = 1;
+                return 1;
+            }
+
+            sign = 1;
+            return 0;
+        }
+    }
+}
---
+++ Task2.Tests/StringConverterTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using Task2.StringConverterLibrary;
+
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class StringConverterTests
+    {
+        [TestCase("-10", -10)]
+        [TestCase("11", 11)]
+        [TestCase("+1", 1)]
+        [TestCase("0", 0)]
+        [TestCase("-1134", -1134)]
+        [TestCase("1534", 1534)]
+        [TestCase("+1234", 1234)]
+        [TestCase("+2147483647", int.MaxValue)]
+        [TestCase("-2147483648", int.MinValue)]
+        [Test]
+        public void ValidIntString_ParsedIntResult(string number, int expected)
+        {
+            int actual = number.ToInt();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        [Test]
+        public void InvalidArgument_ArgumentExceptionThrown(string number)
+        {
+            Assert.Throws<ArgumentException>(() => number.ToInt());
+        }
+
+        [TestCase("21474836485")]
+        [TestCase("-21474836494")]
+        [Test]
+        public void TooBigNumber_OverflowExceptionThrown(string number)
+        {
+            Assert.Throws<OverflowException>(() => number.ToInt());
+        }
+
+        [TestCase("++111")]
+        [TestCase("--111")]
+        [TestCase("11.53")]
+        [TestCase("1,83")]
+        [TestCase("dfsdfsd")]
+        [TestCase("15 + 12")]
+        [Test]
+        public void InvalidNumberFormat_FormatExceptionThrown(string number)
+        {
+            Assert.Throws<FormatException>(() => number.ToInt());
+        }
+
+    }
+}
